018bafefc451802bbe8611301ce3b35793357464
MattKohn <kohney@hotmail.com>
2021-10-19 14:50:04 -0600

    Narrow down target data to capture on submit

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const baseURLpre = 'https://api.coingecko.com/api/v3/simple/price?ids=';
-const baseURLpost = '&vs_currencies=usd';
-const options = {
-  method: 'GET',
-  mode: 'no-cors',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-}
+// API docs: https://www.coingecko.com/en/api/documentation
+const baseURLpre = 'https://api.coingecko.com/api/v3/coins/';
+const baseURLpost = '/tickers?fields=market';
+const coinsListURL = 'https://api.coingecko.com/api/v3/coins/cardano/tickers/binance';
+let tickerContainer = [];
+
+
+
 const searchForm = document.getElementById("search-form");
 const input = document.getElementById("search-input");
 
@@ -14,13 +13,34 @@ searchForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  console.log(input.value);
+
   fetch(baseURLpre + input.value + baseURLpost)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(targetAPIData)
+}
+
+function targetAPIData(data) {
+  // Push the json object to tickerContainer
+  tickerContainer.push(data);
+  // Narrow down to the data we want to use
+  tickerContainer.find(ticker => {
+    let obj = ticker.tickers;
+    let targetObj = obj.filter(e => e.market.name === "Binance");
+    console.log(targetObj[0]);
+  })
 }
 
 
+
+
+
+// const options = {
+//   method: 'GET',
+//   mode: 'no-cors',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+// }
 // function getCrypto() {
 //   fetch(API_URL, options)
 //     .then(response => console.log(response.json()))
