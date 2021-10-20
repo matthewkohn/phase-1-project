75d777c044807898be1a0854c092f08656332e34
MattKohn <kohney@hotmail.com>
2021-10-19 19:07:29 -0600

    Format timestamp, and correct dropdown functionality

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
+// DECLARE VARIABLES
 // API docs: https://www.coingecko.com/en/api/documentation
 const baseURLpre = 'https://api.coingecko.com/api/v3/coins/';
 const baseURLpost = '/tickers?fields=market';
 const coinsListURL = 'https://api.coingecko.com/api/v3/coins/cardano/tickers/binance';
-let tickerContainer = [];
+// Global target object & coin container
 let targetObject;
-
-
-const searchForm = document.getElementById('search-form');
-const input = document.getElementById('search-input');
 const coinContainer = document.getElementById('coin-container');
 
-searchForm.addEventListener('submit', handleSubmit);
+// Fetch data for Search Form selection
+const searchForm = document.getElementById('search-form');
 
-function handleSubmit(event) {
-  event.preventDefault();
+const selectedCoinList = document.getElementById('coins-dropdown');
+selectedCoinList.addEventListener('change', handleSelection);
+
+function handleSelection(event) {
+  event.preventDefault();  
+  // searchForm.reset();
+  // Clear the DOM for each selection
+  removeAllChildNodes(coinContainer);
+  // Ignore invalid selection
+  if (event.target.value === undefined) {
+    return;
+  }
+
+  const input = selectedCoinList.options[selectedCoinList.selectedIndex];
 
   fetch(baseURLpre + input.value + baseURLpost)
     .then(response => response.json())
-    .then(data => {
-      targetAPIData(data);
-      searchForm.reset();
-    })
+    .then(data => targetAPIData(data))
+    .catch(error => console.log(error))
 }
 
-// Handle target data and store in targetObject
+  // Handle target data and store in targetObject
 function targetAPIData(data) {
   // Push the json object to tickerContainer
+  let tickerContainer = [];
   tickerContainer.push(data);
   let targetObj;
   // Narrow down to the data we want to use
@@ -39,59 +48,73 @@ function targetAPIData(data) {
 
 // Make it appear on the screen
 function displayData(obj) {
-  // Create HTML elements
+  // Get the coin container that displays selected coin data
   // Price
-  const h2 = document.createElement('h2');
-  h2.id = 'last-price';
-  h2.innerText = obj.last;
-
-  // Name of coin
-  const h3 = document.createElement('h3');
-  h3.id = 'coin-name';
-  h3.innerText = obj.coin_id;
-  
-  // Link to Binance for this coin
-  const coinName = capitalizeFirstLetter(obj.coin_id);
-  const link = document.createElement('a');
-  link.id = 'trade-link';
-  link.href = obj.trade_url;
-  link.innerText = `Buy or sell ${coinName}!`
-  
+  console.log(obj)
   // Coin symbol
   const symbol = document.createElement('span');
   symbol.id = 'symbol';
   symbol.innerText = obj.base;
-  
-  coinContainer.append( symbol, h2, h3, link );
-  // console.log(h2, h3, link, symbol);
-  
+  // Price
+  let price = obj.last;
+  if (price < 1) {
+    price = obj.last;
+  } else {
+    price.toFixed(2);
+  }
+  const h2 = document.createElement('h2');
+  h2.id = 'last-price';
+  h2.innerText = `$${price}`;
+  // Name of coin
+  const coinName = capitalizeFirstLetter(obj.coin_id);
+  const h3 = document.createElement('h3');
+  h3.id = 'coin-name';
+  h3.innerText = coinName;
+  // Timestamp
+  const time = getTimeFromDate(obj.timestamp);
+  const timestamp = document.createElement('span');
+  timestamp.id = 'time';
+  timestamp.innerText = `Price last updated at ${time}`
+  // Link to CoinGecko for this coin
+  const coinURL = `https://www.coingecko.com/en/coins/${obj.coin_id}`
+  const link = document.createElement('a');
+  link.id = 'trade-link';
+  link.href = coinURL;
+  link.innerText = `Find out more about ${coinName} here!`
+  link.target = "_blank"
+  // Add coin data to the container
+  coinContainer.append( symbol, h2, h3, timestamp, link );
 }
 
-// Borrowed helper function from https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+// Function to clear the Coin Container in the DOM:
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
+
+// Capitalize name of coin function
+// Source: https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-/* 
-  targetObject.last
+// Convert timestamp to readable time for DOM
+// Source: https://stackoverflow.com/questions/40927938/extract-time-from-timestamp-in-js
+function getTimeFromDate(timestamp) {
+  const pad = num => ("0" + num).slice(-2);
+  // Format from JSON: "2021-10-20T00:04:26+00:00"
+  const date = new Date(timestamp);
+  // figure out AM or PM
+  let hours, amOrPm;
+  if (date.getHours() >= 12) {
+    amOrPm = 'pm';
+    hours = date.getHours() % 12;
+  } else {
+    amOrPm = 'am';
+    hours = date.getHours();
+  }
+  const minutes = date.getMinutes();
+  return `${hours}:${pad(minutes)} ${amOrPm}`
+}
 
-*/
-
-
-
-
-
-// const options = {
-//   method: 'GET',
-//   mode: 'no-cors',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-// }
-// function getCrypto() {
-//   fetch(API_URL, options)
-//     .then(response => console.log(response.json()))
-//     .then(result => console.log(result));
-// }
-
-// document.addEventListener('DOMContentLoaded', getCrypto);
