554b37d31de9080cf4d04eb162b28c1f4ce350a5
MattKohn <kohney@hotmail.com>
2021-11-02 11:30:42 -0600

    Solved element format functions

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // API docs: https://www.coingecko.com/en/api/documentation
 
 /*---------------- LOADING PAGE ------------------*/
-// Global Coin Container variable used in load, fetch, and to append elements created using API data
-const coinContainer = document.getElementById('coin-container');
-
 // Display the dropdown and default images when DOM Content Loads
 document.addEventListener('DOMContentLoaded', init);
 
@@ -39,6 +36,7 @@ function displayErrorMessage() {
 function loadStartImages() {
   const stockImageURL = 'https://images.unsplash.com/photo-1515879128292-964efc3ebb25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80';
   const coingeckoImgURL = 'https://static.coingecko.com/s/coingecko-branding-guide-4f5245361f7a47478fa54c2c57808a9e05d31ac7ca498ab189a3827d6000e22b.png';
+  const coinContainer = document.getElementById('coin-container');
   const coingeckoLink = document.getElementById('coingecko-link');
   // Loads a stock image inside the Coin Container until a coin is chosen from the dropdown.
   loadImage(stockImageURL, 'placeholder-image', 'Green bottle on the edge of a sandy beach.', coinContainer);
@@ -106,8 +104,6 @@ function handleDropdownSelection(event, apiData) {
   // Prevent default & capture target value
   event.preventDefault();  
   const targetValue = event.target.value;
-  // Clear the DOM for each selection
-  removeAllChildNodes(coinContainer);
   // Display the selected cryptocurrency's data in the DOM
   displayData(apiData, targetValue);
   // console.log(apiData);
@@ -121,168 +117,79 @@ function removeAllChildNodes(parent) {
 }
 
 function displayData(apiData, selectedValue) {
+  // const elementArray = [ nameEl, symbolEl, priceEl, high24El, low24El, rankEl, marketCapEl, supplyEl, volumeEl ];
+  const coinContainer = document.getElementById('coin-container');
+  // Clear the DOM for each selection
+  removeAllChildNodes(coinContainer);
+
   // Find the selectedValue inside apiData
   const chosenCoin = apiData.find(coin => coin.id === selectedValue);
 
-
   // loadImage param format: (url, assignClass, alt, appendTarget) from line 49
-  const coinImage = loadImage(
-    chosenCoin.image,
-    'coin-image', 
-    `${chosenCoin.name} logo`, 
-    coinContainer);
-
+  const coinImage = loadImage( chosenCoin.image, 'coin-image', `${chosenCoin.name} logo`, coinContainer);
   
-  // const elementArray = [ nameEl, symbolEl, priceEl, high24El, low24El, rankEl, marketCapEl, supplyEl, volumeEl ];
-
-
-  // createCoinEl param format: (tagNameStr, idStr, coinObj, apiValueStr, formatFunc) 
+  // createCoinEl param format: (coinObj, tagNameStr, idStr, apiValueStr, formatType(*OPTIONAL)) 
   const nameEl = createCoinEl(chosenCoin, 'h3', 'coin-name', 'name');
   const symbolEl = createCoinEl(chosenCoin, 'span', 'symbol', 'symbol');
+  const priceEl = createCoinEl(chosenCoin, 'h2', 'price', 'current_price', 'price');
+  const high24El = createCoinEl(chosenCoin, 'span', 'high-24', 'high_24h', 'price');
+  const low24El = createCoinEl(chosenCoin, 'span', 'low-24', 'low_24h', 'price');
+  const rankEl = createCoinEl(chosenCoin, 'span', 'rank', 'market_cap_rank');
+
+  const marketCapEl = createCoinEl(chosenCoin, 'span', 'market-cap', 'market_cap', 'price');
+  const supplyEl = createCoinEl(chosenCoin, 'span', 'supply', 'circulating_supply', 'bigNumber');
+  const volumeEl = createCoinEl(chosenCoin, 'span', 'volume', 'total_volume', 'bigNumber');
+  
+  const timeEl = createCoinEl(chosenCoin, 'span', 'time', 'last_updated', 'date')
+  // Append
 
 
-  const priceEl = createCoinEl(chosenCoin, 'h2', 'price', 'current_price');
-  formatPrice(chosenCoin, priceEl, 'current_price');
-  
-  console.log(priceEl);
-  
+
+  console.log(nameEl, symbolEl, priceEl, high24El, low24El, rankEl, marketCapEl, supplyEl, volumeEl, timeEl)
 }
 
-// function createMoneyEl(selectedCoin, )
-
-
+/*------------------------ COIN CONTAINER DOM ELEMENTS --------------------*/
 // ELEMENT CREATOR FUNCTION
-function createCoinEl(coinObj, tagNameStr, idStr, apiValueStr) {
+function createCoinEl(coinObj, tagNameStr, idStr, apiValueStr, formatType) {
   const element = document.createElement(tagNameStr);
-  Object.assign(element, {
-    id: idStr,
-    textContent: `${coinObj[apiValueStr]}`,
-    function: 
-  });
+  element.id = idStr;
+  // Switch handles the optional "formatType" parameter
+  switch(formatType) {
+    case 'price':
+      formatPrice(coinObj, apiValueStr, element);
+      break;
+    case 'bigNumber':
+      formatLargeNumber(coinObj, apiValueStr, element);
+      break;
+    case 'date':
+      formatDate(coinObj, apiValueStr, element);
+      break;
+    default:
+      element.textContent = coinObj[apiValueStr];
+  }
   return element;
 }
 
 // Formats how prices are displayed in the DOM
-function formatPrice(coinObj, elName, apiValueStr) {
+function formatPrice(coinObj, apiValueStr, elName) {
   let price = coinObj[apiValueStr];
   // Prices over a dollar are shown with 2 numbers after the decimal. 
   if (price >= 1) {
-    price = formatLargeNumber(price);
+    price = price.toLocaleString(undefined, { style: 'currency', currency: 'USD' });
+  } else {
+    price = `$${price}`
   }
-  elName.textContent = `$${price}`; 
+  elName.textContent = price; 
 }
 
-// const high24El = createCoinEl('span', 'high-24', chosenCoin, 'high_24h');
-// const low24El = createCoinEl('span', 'low-24', chosenCoin, 'low_24h');
-// const marketCapEl = createCoinEl('span', 'market-cap', chosenCoin, 'market_cap');
-
-// const rankEl = createCoinEl('span', 'rank', chosenCoin, 'market_cap_rank');
-// const supplyEl = createCoinEl('span', 'supply', chosenCoin, 'circulating_supply');
-
-// const volumeEl = createCoinEl('span', 'volume', chosenCoin, 'total_volume');
-// formatVolume(volumeEl);
-/*    
-last_updated  (date)
-span, format date
-*/
-// formatPrice(chosenCoin, high24El, 'high_24h');
-  // formatPrice(chosenCoin, low24El, 'low_24h');
-  // formatPrice(chosenCoin, marketCapEl, 'market_cap');
-  // formatPrice(chosenCoin, supplyEl, 'circulating_supply');
-  
-  // console.log(nameEl);
-  // console.log(symbolEl);
-  // console.log(priceEl);
-  // console.log(high24El);
-  // console.log(low24El);
-  // console.log(rankEl);
-  // console.log(marketCapEl);
-  // console.log(supplyEl);
-  // console.log(volumeEl);
-
-  // // console.log(apiData);
-  // console.log(selectedValue);
-  // console.log(chosenCoin);
-
-  
-// Formats the Volume Element
-function formatVolume(volEl) {
-  let volContent = volEl.textContent;
-  volContent = volContent.split('.')
-  volContent[0] = parseInt(volContent, 10);
-  volContent[0] = formatLargeNumber(volContent);
-  return volEl.textContent = volContent[0];
+// Formats large numbers that aren't currency
+function formatLargeNumber(coinObj, apiValueStr, elName) {
+  let bigNum = coinObj[apiValueStr];
+  elName.textContent = bigNum.toLocaleString(undefined);
 }
 
-
-// Formats numbers with 2 decimal places and commas as necessary for large numbers
-// used in formatPrice() & formatVolume()
-function formatLargeNumber(number) {
-  return number.toLocaleString(undefined, { minimumFractionDigits: 2 } );
+// Formats timestamp received from the API to a readable local date & time
+function formatDate(coinObj, apiValueStr, elName) {
+  const timestamp = new Date(coinObj[apiValueStr]);
+  elName.textContent = timestamp;
 }
-// SOURCE: https://stackoverflow.com/questions/5731193/how-to-format-numbers
-
-
-function formatDate(coin) {
-  
-}
-
-
-/*----------------------------- DROPDOWN API FUNCTIONALITY -------------------------*/
-
-  
-  // Update the coin container, displaying selected coin data
-  //   function displayData(obj) {
-//     
-
-//   // TIMESTAMP
-//   const time = getTimeFromDate(obj.timestamp);
-//   const timestamp = document.createElement('span');
-//   timestamp.id = 'time';
-//   timestamp.innerText = `Price last updated at ${time}`
-//   // LINK to CoinGecko for this coin
-//   const coinURL = `https://www.coingecko.com/en/coins/${obj.coin_id}`
-//   const link = document.createElement('a');
-//   link.id = 'trade-link';
-//   link.href = coinURL;
-//   link.innerText = `Find out more about ${coinName} here!`
-//   link.target = "_blank"
-//   link.addEventListener("mouseover", activateLinkButton, false);
-//   link.addEventListener("mouseout", deactivateLinkButton, false);
-//   // ADD coin data to the coin container
-//   coinContainer.append( symbol, h2, h3, timestamp, link );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*------------------------- Re-usable Functions -----------------------*/
-
-// Convert timestamp to readable time for DOM
-// Adapted from source: https://stackoverflow.com/questions/40927938/extract-time-from-timestamp-in-js
-
-// Souce: https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
-function formatAMPM(timestamp) {
-  // Format from JSON: "2021-10-20T00:04:26+00:00"
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  let strTime = hours + ':' + minutes + ' ' + ampm;
-  return strTime;
-}
-console.log(formatAMPM("2021-10-20T00:04:26+00:00"))
